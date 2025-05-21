6fdf261191717d6192e28e3a14d362c5a08d79a8
Nacho <barrojojoaquin2@gmail.com>
2025-05-21 20:29:33 -0300

    agregamos la clase contacto y app puede crear objetos de tipo contactos

--- js/app.js
+++ js/app.js
@@ -1,9 +1,31 @@
+import Contacto from "./crearContacto.js"
+
+//funciones
 const abrirModal = ()=>{
 const modalContacto = new bootstrap.Modal(document.getElementById('modalContacto'))
 //aqui abro la ventana modal
 modalContacto.show()
 }
 
-const btnAgregar = document.getElementById('btnAgregar')
+const creaContacto=()=>{
+    //todo:tomar los datos del formulario y validar
+    //con los datos voy a crear un objeto contacto
+    const nuevoContacto = new Contacto('Darth', 'Vader','4424454','','')
+    console.log(nuevoContacto);
+    
+    //guardar el contacto en un array
+}
 
+//declarar variables
+const btnAgregar = document.getElementById('btnAgregar')
+const formularioContacto= document.querySelector('form')
+const agenda = []
+
+//agrego los manejadores de eventos
 btnAgregar.addEventListener('click', abrirModal)
+formularioContacto.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    //Aqui creare un contacto
+    creaContacto()
+    //algun dia voy a editar un contacto
+})
