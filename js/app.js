d698783a0b4e8931d7065f8ce32c32672906f1c9
Nacho <barrojojoaquin2@gmail.com>
2025-05-21 22:03:41 -0300

    Agregamos localStorage y JSON para guardar datos de un array sin perderlo

--- js/app.js
+++ js/app.js
@@ -10,16 +10,33 @@ modalContacto.show()
 const creaContacto=()=>{
     //todo:tomar los datos del formulario y validar
     //con los datos voy a crear un objeto contacto
-    const nuevoContacto = new Contacto('Darth', 'Vader','4424454','','')
+    const nuevoContacto = new Contacto(inputNombre.value, inputApellido.value, inputTel.value,inputEmail.value,inputImg.value,inputNota.value)
     console.log(nuevoContacto);
-    
+    agenda.push(nuevoContacto)
+    limpiarForm()
+    //guardar la agenda en local storage
+    guardarLocalStorage()
     //guardar el contacto en un array
+}
+
+const limpiarForm =()=>{
+    formularioContacto.reset()
+}
+
+const guardarLocalStorage =()=>{
+    localStorage.setItem('agendaKey', JSON.stringify(agenda))
 }
 
 //declarar variables
 const btnAgregar = document.getElementById('btnAgregar')
 const formularioContacto= document.querySelector('form')
-const agenda = []
+const inputNombre = document.getElementById('nombre')
+const inputApellido = document.getElementById('apellido')
+const inputEmail = document.getElementById('email')
+const inputTel = document.getElementById('telefono')
+const inputImg = document.getElementById('imagen')
+const inputNota = document.getElementById('notas')
+const agenda = JSON.parse(localStorage.getItem('agendaKey')) || []
 
 //agrego los manejadores de eventos
 btnAgregar.addEventListener('click', abrirModal)
